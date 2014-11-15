505e9ff2677180fff97c7d479f5303578c6feb18
Aaron Schlesinger <aschlesinger@paypal.com>
2014-11-14 16:34:33 -0800

    filling out the template adapter more

--- html/template/template.go
+++ html/template/template.go
@@ -2,6 +2,7 @@ package templatefs
 
 import (
   "html/template"
+  "io"
 )
 
 type AssetFunc func(string) ([]byte, error)
@@ -15,15 +16,52 @@ func New(name string, fn AssetFunc) *Template {
   return &Template{fn, template.New(name)}
 }
 
+func (t *Template) Name() string {
+  return t.tmpl.Name()
+}
+
+func (t *Template) Funcs(funcMap template.FuncMap) *Template {
+  return t.tmpl.Funcs(funcMap)
+}
+
 func (t *Template) Parse(filename string) (*Template, error) {
-  tmplBytes, err := Asset(fileName)
+  tmplStr, err := t.file(filename)
   if err != nil {
     return nil, err
   }
-  tmplStr := string(tmplBytes)
   newTmpl, err := t.tmpl.Parse(tmplStr)
   if err != nil {
     return nil, err
   }
   t.tmpl := newTmpl
+  return t, nil
+}
+
+func (t *Template) ParseFiles(filenames ...string) (*Template, error) {
+  fileStrs := []string{}
+  for _, filename := range filenames {
+    fileStr, err := t.file(filename)
+    if err != nil {
+      return nil, err
+    }
+    fileStrs = append(fileStrs, fileStr)
+  }
+  newTmpl, err := t.tmpl.ParseFiles(fileStrs...)
+  if err != nil {
+    return nil, err
+  }
+  t.tmpl = newTmpl
+  return t, nil
+}
+
+func (t *Template) Execute(w io.Writer, data interface{}) error {
+  return t.tmpl.Execute(w, data)
+}
+
+func (t *Template) file(name string) ([]byte, error) {
+  tmplBytes, err := Asset(fileName)
+  if err != nil {
+    return nil, err
+  }
+  return string(tmplBytes), nil
 }
